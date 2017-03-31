a0f8207fdbd730b445c1ea9e0c5cb28449a1c43f
YuriiHoliuk <bionicuss@gmail.com>
2017-03-31 22:30:41 +0300

    add shouldSayHello specs

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ let bubbleSort = require('./bubble-sort');
 let coverByTile = require('./entry-test/coverByTile');
 let divideWatermelon = require('./divideWatermelon');
 let countStones = require('./countStones');
+let shouldSayHello = require('./shouldSayHello');
 
 let algolib = {
     bubbleSort,
     coverByTile,
     divideWatermelon,
-    countStones
+    countStones,
+    shouldSayHello
 }
 
 module.exports = algolib;
