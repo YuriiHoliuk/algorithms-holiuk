59a93d7878b07eb03cd8935c7b41d579d0a677e8
YuriiHoliuk <bionicuss@gmail.com>
2017-03-31 22:49:17 +0300

    add getNext BeautifulYear specs

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ let coverByTile = require('./entry-test/coverByTile');
 let divideWatermelon = require('./divideWatermelon');
 let countStones = require('./countStones');
 let shouldSayHello = require('./shouldSayHello');
+let getNextBeautifulYear = require('./getNextBeautifulYear');
 
 let algolib = {
     bubbleSort,
     coverByTile,
     divideWatermelon,
     countStones,
-    shouldSayHello
+    shouldSayHello,
+    getNextBeautifulYear
 }
 
 module.exports = algolib;
