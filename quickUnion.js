5f4c344ab55ffd18a5b64fc62668c6a2e439cf8d
YuriiHoliuk <bionicuss@gmail.com>
2017-04-16 12:43:04 +0300

    correct algorithm (weights changes) & add path compression

--- quickUnion.js
+++ quickUnion.js
@@ -12,19 +12,16 @@ class QuickUnion {
             this._elements[a] = a;
             this._weights[a] = 1;
         }
+
         if (isNaN(this._elements[b])) {
             this._elements[b] = b;
             this._weights[b] = 1;
         }
 
-        let rootA = this.findRoot(a);
-        let rootB = this.findRoot(b);
+        let [isConnected, rootA, rootB] = this.isConnected(a, b, true);
 
-        this._elements[a] = rootA;
-        this._elements[b] = rootB;
-
-        if (rootA === rootB) {
-            console.log('these elements ic connected');
+        if (isConnected) {
+            console.log('these elements is connected');
             return;
         }
 
@@ -32,49 +29,49 @@ class QuickUnion {
         let weightB = this._weights[rootB];
 
         if (weightA <= weightB) {
-            this._elements[a] = rootB;
-            this._weights[rootB]++;
+            this._elements[rootA] = rootB;
+            this._weights[rootB] += this._weights[rootA];
         } else {
-            this._elements[b] = rootA;
-            this._weights[rootA]++;
+            this._elements[rootB] = rootA;
+            this._weights[rootA] += this._weights[rootB];
         }
     }
 
-    isConnected(a, b) {
+    isConnected(a, b, service) {
         if (isNaN(this._elements[a]) || isNaN(this._elements[b])) {
             return false;
         } else {
-            let rootA = this.findRoot(a);
-            let rootB = this.findRoot(b);
+            let rootA = this._findRoot(a);
+            let rootB = this._findRoot(b);
 
             this._elements[a] = rootA;
             this._elements[b] = rootB;
 
-            return rootA === rootB;
+            return service ? [rootA === rootB, rootA, rootB] : rootA === rootB;
         }
     }
 
-    findRoot(e) {
-        return e === this._elements[e] ? e : this.findRoot(this._elements[e]);
+    _findRoot(e) {
+        return e === this._elements[e] ? e : this._findRoot(this._elements[e]);
     }
 
-    count() {
-        let uniq = [];
-        for (let i = 0; i < this._elements.length; i++) {
-            if (this._elements[i] !== undefined && !~uniq.indexOf(this._elements[i])) {
-                uniq.push(this._elements[i]);
-            }
-        }
-        return uniq.length;
-    }
-
-    component(a) {
-        if (isNaN(this._elements[a])) {
-            return false;
-        } else  {
-            return this._elements[a];
-        }
-    }
+    // count() {
+    //     let uniq = [];
+    //     for (let i = 0; i < this._elements.length; i++) {
+    //         if (this._elements[i] !== undefined && !~uniq.indexOf(this._elements[i])) {
+    //             uniq.push(this._elements[i]);
+    //         }
+    //     }
+    //     return uniq.length;
+    // }
+    //
+    // component(a) {
+    //     if (isNaN(this._elements[a])) {
+    //         return false;
+    //     } else  {
+    //         return this._elements[a];
+    //     }
+    // }
 }
 
 module.exports = QuickUnion;
--- test/quickUnion.spec.js
+++ test/quickUnion.spec.js
@@ -9,12 +9,12 @@ describe.only('#QuickUnion', function() {
 
         qu.connect.should.be.instanceOf(Function);
         qu.isConnected.should.be.instanceOf(Function);
-        qu.count.should.be.instanceOf(Function);
-        qu.component.should.be.instanceOf(Function);
+        // qu.count.should.be.instanceOf(Function);
+        // qu.component.should.be.instanceOf(Function);
     });
 
 
-    it('should  #isConnected return false if not connected', function() {
+    it('should #isConnected return false if not connected', function() {
         let qu = new algolib.QuickUnion();
 
         let isElementConnected = qu.isConnected(8, 15);
@@ -22,7 +22,7 @@ describe.only('#QuickUnion', function() {
     });
 
 
-    it('should  #isConnected return true if elements are connected', function() {
+    it('should #isConnected return true if elements are connected', function() {
         let qu = new algolib.QuickUnion();
 
         qu.connect(2, 10);
@@ -31,7 +31,7 @@ describe.only('#QuickUnion', function() {
         isElementConnected.should.be.true();
     });
 
-    it('should  #isConnected return true if elements are connected in chain', function() {
+    it('should #isConnected return true if elements are connected in chain', function() {
         let qu = new algolib.QuickUnion();
 
         qu.connect(2, 10);
@@ -41,104 +41,123 @@ describe.only('#QuickUnion', function() {
         isElementConnected.should.be.true();
     });
 
-
-    it('should #count return 0 if no used elements', function() {
+    it('should #isConnected change weights[a] to 1 & weights[b] to 2 if a & b was never connected', function() {
         let qu = new algolib.QuickUnion();
 
+        qu.connect(2, 10);
 
-        let count = qu.count();
-        count.should.be.eql(0);
+        qu._weights[2].should.be.eql(1);
+        qu._weights[10].should.be.eql(2);
     });
 
-    it('should #count return 1 if we connet 2 elements to 1 component', function() {
+    it('should #isConnected change weights[a] to 3 if connect chain c > b > a ', function() {
         let qu = new algolib.QuickUnion();
-
-        qu.connect(9, 19);
-
-        let count = qu.count();
-        count.should.be.eql(1);
-    });
-
-    it('should #count return 2 if we conneted 2 times and create 2 components', function() {
-        let qu = new algolib.QuickUnion();
-
-        qu.connect(9, 19);
-        qu.connect(8, 1);
-
-
-        let count = qu.count();
-        count.should.be.eql(2);
-    });
-
-    it('should #component return "false" if element are not in any component ', function() {
-        let qu = new algolib.QuickUnion();
-
-        let component = qu.component(5);
-        component.should.be.false();
-    });
-
-    it('should #component(1) return "5" if element connected to 5', function() {
-        let qu = new algolib.QuickUnion();
-
-        qu.connect(1, 5);
-
-        let component = qu.component(1);
-        component.should.be.eql(5);
-    });
-
-    it('should #component(1) return "10" after chain: 1 > 2 > 4 > 7 > 10 (revers)', function() {
-        let qu = new algolib.QuickUnion();
-
-        qu.connect(7, 10);
-        qu.connect(4, 7);
-        qu.connect(2, 4);
-        qu.connect(1, 2);
-
-        let component = qu.component(1);
-        component.should.be.eql(10);
-    });
-
-
-    it('should somesing', function() {
-        let qu = new algolib.QuickUnion();
-
-        qu.connect(7, 10);
-        console.log(qu._elements);
-
-        qu.connect(4, 7);
-        console.log(qu._elements);
-
-        qu.connect(2, 4);
-        console.log(qu._elements);
 
         qu.connect(1, 2);
-        console.log(qu._elements);
+        qu.connect(3, 1);
 
-
-
-        qu.connect(19, 26);
-        console.log(qu._elements);
-
-        qu.connect(13, 20);
-        console.log(qu._elements);
-
-        qu.connect(13, 22);
-        console.log(qu._elements);
-
-        qu.connect(22, 26);
-        console.log(qu._elements);
-        console.log(qu._elements[19]);
-
-        qu.connect(19, 4);
-        console.log(qu._elements);
-
-
-        console.log(qu.isConnected(19, 26));
-        console.log(qu.isConnected(19, 1));
-        console.log(qu.isConnected(26, 1));
-
-        let t = true;
-        t.should.be.true();
+        qu._weights[2].should.be.eql(3);
     });
+
+    it('should #isConnected(a, c) return true if connect chain c > b > a ', function() {
+        let qu = new algolib.QuickUnion();
+
+        qu.connect(1, 2);
+        qu.connect(3, 1);
+
+        qu.isConnected(2, 3).should.be.true();
+    });
+
+    it('should weights[5] be equal 5 after chain connection: 7 > 5; 8 > 7; 6 > 4; 7 > 6', function() {
+        let qu = new algolib.QuickUnion();
+
+        qu.connect(7, 5);
+        qu.connect(8, 7);
+        qu.connect(6, 4);
+        qu.connect(7, 6);
+
+        qu._weights[5].should.be.eql(5);
+    });
+
+    it('should findRoot(6) be equal 5 after chain connection: 7 > 5; 8 > 7; 6 > 4; 7 > 6', function() {
+        let qu = new algolib.QuickUnion();
+
+        qu.connect(7, 5);
+        qu.connect(8, 7);
+        qu.connect(6, 4);
+        qu.connect(7, 6);
+
+        qu._findRoot(6).should.be.eql(5);
+    });
+
+    it('should isConnected(6, 5) change elements[6] to 5 after chain connection: 7 > 5; 8 > 7; 6 > 4; 7 > 6', function() {
+        let qu = new algolib.QuickUnion();
+
+        qu.connect(7, 5);
+        qu.connect(8, 7);
+        qu.connect(6, 4);
+        qu.connect(7, 6);
+
+        qu.isConnected(6, 5);
+
+        qu._elements[6].should.be.eql(5);
+    });
+
+
+    // it('should #count return 0 if no used elements', function() {
+    //     let qu = new algolib.QuickUnion();
+    //
+    //
+    //     let count = qu.count();
+    //     count.should.be.eql(0);
+    // });
+    //
+    // it('should #count return 1 if we connet 2 elements to 1 component', function() {
+    //     let qu = new algolib.QuickUnion();
+    //
+    //     qu.connect(9, 19);
+    //
+    //     let count = qu.count();
+    //     count.should.be.eql(1);
+    // });
+    //
+    // it('should #count return 2 if we conneted 2 times and create 2 components', function() {
+    //     let qu = new algolib.QuickUnion();
+    //
+    //     qu.connect(9, 19);
+    //     qu.connect(8, 1);
+    //
+    //
+    //     let count = qu.count();
+    //     count.should.be.eql(2);
+    // });
+    //
+    // it('should #component return "false" if element are not in any component ', function() {
+    //     let qu = new algolib.QuickUnion();
+    //
+    //     let component = qu.component(5);
+    //     component.should.be.false();
+    // });
+    //
+    // it('should #component(1) return "5" if element connected to 5', function() {
+    //     let qu = new algolib.QuickUnion();
+    //
+    //     qu.connect(1, 5);
+    //
+    //     let component = qu.component(1);
+    //     component.should.be.eql(5);
+    // });
+    //
+    // it('should #component(1) return "10" after chain: 1 > 2 > 4 > 7 > 10 (revers)', function() {
+    //     let qu = new algolib.QuickUnion();
+    //
+    //     qu.connect(7, 10);
+    //     qu.connect(4, 7);
+    //     qu.connect(2, 4);
+    //     qu.connect(1, 2);
+    //
+    //     let component = qu.component(1);
+    //     component.should.be.eql(10);
+    // });
 
 });
