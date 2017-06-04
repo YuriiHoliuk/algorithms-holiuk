a91f4d5e288889d9c94b8eb82a736100bde8704a
YuriiHoliuk <bionicuss@gmail.com>
2017-06-04 15:51:17 +0200

    add general sorts performance test & write test results to JSON

--- algorithms/sort-algorithms/sorts.spec.js
+++ algorithms/sort-algorithms/sorts.spec.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const fs = require('fs');
+
 const algolib = require('../../index');
 const sorts = {
     bubbleSort: algolib.bubbleSort,
@@ -11,35 +13,103 @@ const sorts = {
     heapSort: algolib.heapSort
 };
 
-const compare =  require('../utilities/compare-functions').compareNumbers;
+const compare = require('../utilities/compare-functions').compareNumbers;
 
 const swap = require('../utilities/utilities').swap;
 const shuffle = require('../utilities/utilities').shuffleArray;
 const isSorted = require('../utilities/utilities').isSorted;
 const getShuffledArray = require('../utilities/utilities').getShuffledArray;
+const makePartialSorted = require('../utilities/utilities').makePartialSorted;
 
-function makePartialSorted(array, k, d) {
-    let length = array.length,
-        times = length * k;
-        console.log(times);
+let length = 10000;
+let results = {
+    sorts: [],
+    timers: {
+        shuffledTime: {},
+        partialTime: {},
+        reverseTime: {},
+        sortedTime: {}
+    },
+    compares: {}
+};
 
-    for (let i = 0; i < times; i++) {
-        let a = random(0, length - 1),
-            b = random(a - times * d, a + times * d);
-
-        if (array[b]) {
-            swap(array, a, b);
-        }
-    }
-
-    return array;
+for (let sortName in sorts) {
+    results.sorts.push(sortName);
 }
 
-describe('#All sorts performance', function () {
+describe.only(`#All sorts performance for ${length} elements`, function () {
     this.timeout(0);
 
-    it('shood return array', function () {
-        let testArray = getShuffledArray(10);
-        testArray.should.be.instanceOf(Array);
+    after(function () {
+        console.log(results);
+        let resultsJSON = JSON.stringify(results, null, 4);
+        fs.writeFile('./algorithms/sort-algorithms/overall-performance.json', resultsJSON);
     });
+
+    for (let sort in sorts) {
+        describe(`#${sort}`, function () {
+            let testArray = getShuffledArray(length);
+
+            it('', function () {
+                this.slow(9999999999);
+
+                let start = new Date();
+                testArray = sorts[sort](testArray, compare);
+                let end = new Date();
+                let result = end - start;
+                results.timers.shuffledTime[sort] = result;
+
+                let compares = compare.count;
+                results.compares[sort] = compares;
+                compare.resetCount();
+
+                isSorted(testArray, compare).should.be.true();
+                console.log(`        Sort shuffled array ${result}ms`);
+            });
+
+            it('', function () {
+                this.slow(9999999999);
+
+                testArray = makePartialSorted(testArray, 0.4, 0.3);
+
+                let start = new Date();
+                testArray = sorts[sort](testArray, compare);
+                let end = new Date();
+                let result = end - start;
+                results.timers.partialTime[sort] = result;
+
+                isSorted(testArray, compare).should.be.true();
+                console.log(`        Sort partial sorted array ${result}ms`);
+            });
+
+            it('', function () {
+                this.slow(9999999999);
+
+                testArray = testArray.reverse();
+
+                let start = new Date();
+                testArray = sorts[sort](testArray, compare);
+                let end = new Date();
+                let result = end - start;
+                results.timers.reverseTime[sort] = result;
+
+                isSorted(testArray, compare).should.be.true();
+                console.log(`        Sort reversed array ${result}ms`);
+            });
+
+            it('', function () {
+                this.slow(9999999999);
+
+                let start = new Date();
+                testArray = sorts[sort](testArray, compare);
+                let end = new Date();
+                let result = end - start;
+                results.timers.sortedTime[sort] = result;
+
+                isSorted(testArray, compare).should.be.true();
+                console.log(`        Sort sorted array ${result}ms`);
+            });
+
+        });
+    }
 });
