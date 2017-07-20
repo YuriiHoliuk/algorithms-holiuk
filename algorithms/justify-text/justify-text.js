5163dc83b98ab35b8baef3efc84643aa7fbcddcd
YuriiHoliuk <bionicuss@gmail.com>
2017-07-21 00:22:39 +0300

    implement justify-text

--- algorithms/justify-text/justify-text.js
+++ algorithms/justify-text/justify-text.js
@@ -1,6 +1,6 @@
-const Graph = require('../../index');
+const Graph = require('../../index').Graph;
 
-function justifyText(text, pageWidth, lineBrake) {
+function justifyText(text, pageWidth, lineBreak = '\n') {
     let str = text;
 
     str = str.trim().replace(/\s+/g, ' ');
@@ -13,9 +13,45 @@ function justifyText(text, pageWidth, lineBrake) {
     while (result = regexp.exec(str)) {
         vertexes.push(result.index);
     }
+    vertexes.unshift(0);
 
-    console.log(vertexes);
+    const graph = new Graph();
+    calculateUgliness = calculateUgliness.bind(null, pageWidth);
+
+    for (let i = 0; i < vertexes.length - 1; i++) {
+
+        for (let j = i + 1; j < vertexes.length; j++) {
+            const lineWidth = vertexes[j] - vertexes[i];
+            if (lineWidth <= pageWidth + 1) {
+                const ugliness = calculateUgliness(lineWidth);
+                graph.addEdge(vertexes[i], vertexes[j], ugliness);
+            } else {
+                break;
+            }
+        }
+    }
+
+    const shortestPath = graph.shortestPath(0).edgeTo;
+    let lineBreaks = [];
+
+    lineBreaks.push(vertexes.pop());
+
+    while (shortestPath[lineBreaks[lineBreaks.length - 1]]) {
+        lineBreaks.push(shortestPath[lineBreaks[lineBreaks.length - 1]].from());
+    }
+
+    lineBreaks.pop();
+    lineBreaks = lineBreaks.reverse();
+
+    lineBreaks.forEach(lineBreakIndex => {
+        str = str.substr(0, lineBreakIndex) + lineBreak + str.substr(++lineBreakIndex);
+    });
+
     return str;
+
+    function calculateUgliness(pageWidth, lineWidth) {
+        return Math.pow(pageWidth - lineWidth, 3);
+    }
 }
 
 module.exports = justifyText;
--- algorithms/justify-text/justify-text.spec.js
+++ algorithms/justify-text/justify-text.spec.js
@@ -6,16 +6,12 @@ describe.only('#justifyText', function () {
     });
 
     it('should return string', function () {
-        const text = `
-            dfjhdfjdfh    
-            sds ssfjuyjy   sdshhd   dfdf
-            s    kgmf
-                    fkgm
-                    kmkm        
-        `;
+        const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras vel cursus turpis. Sed vel rutrum justo. Nulla dui risus, dapibus porta velit ut, luctus interdum ipsum. Nullam dictum molestie tellus, ut tincidunt libero convallis rhoncus. Phasellus volutpat, erat eget elementum porttitor, libero ipsum sodales est, id commodo orci libero ut risus. Sed non elementum sapien. Phasellus finibus, lacus vel euismod suscipit, dui felis pretium ex, ut consequat diam sapien ut lorem. Donec porta magna a nunc euismod egestas.
 
-        // console.log(text);
-        const str = justifyText(text);
+Morbi mauris odio, dapibus quis dictum ac, malesuada rhoncus ligula. Etiam quam diam, porttitor eu dictum vel, consectetur id eros. Sed convallis.`;
+
+        // console.log(text, '\n\n\n');
+        const str = justifyText(text, 30);
         // console.log(str);
 
         str.should.be.instanceOf(String);
